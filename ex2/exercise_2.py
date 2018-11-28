c21809fedc3b9afb479f42e59c498b86cabee6d0
paulhfu <hiltpa@gmail.com>
2018-11-28 11:54:31 +0100

    ex 2.1 done

--- ex2/exercise_2.py
+++ ex2/exercise_2.py
@@ -5,6 +5,8 @@
 
 from pulp import *
 from pulp.solvers import *
+import fnmatch
+import numpy as np
 
 def convert_to_ilp(nodes, edges):
     prob = LpProblem("test1", LpMinimize)
@@ -16,10 +18,10 @@ def convert_to_ilp(nodes, edges):
     objs = []
     bins = []
     for i in range(len(nodes)):
-        for j in range(len(getattr(nodes[i], "costs"))):
+        for j in range(len(nodes[i].costs)):
             bins.append(LpVariable("u_n"+str(i)+","+str(j), 0, 1, cat=LpInteger))
             # Objective
-            objs.append(bins[j] * getattr(nodes[i], "costs")[j])
+            objs.append(bins[j] * nodes[i].costs[j])
         prob += lpSum(bins) == 1
         u_nodes.append(bins)
         bins = []
@@ -27,25 +29,42 @@ def convert_to_ilp(nodes, edges):
     u_edges = []
     bins = []
     for i in range(len(edges)):
-        for key in getattr(edges[i], "costs"):
-            bins.append(LpVariable("u_e"+str(getattr(edges[i], 'left')) + "," + str(getattr(edges[i], 'right'))+";"+str(key), 0, 1, cat=LpInteger))
+        for key in edges[i].costs:
+            bins.append(LpVariable("u_e"+str(edges[i].left) + "," + str(edges[i].right)+";"+str(key[0])+","+str(key[1])
+                                   , 0, 1, cat=LpInteger))
             # Objective
-            objs.append(bins[-1] * getattr(edges[i], "costs")[key])
-            # Constraints
-            prob += bins[-1] - u_nodes[getattr(edges[i], 'left')][key[0]] == 0
-            prob += bins[-1] - u_nodes[getattr(edges[i], 'right')][key[1]] == 0
+            objs.append(bins[-1] * edges[i].costs[key])
+        bins2 = []
+        for label in range(len(nodes[edges[i].left].costs)):
+            for var in bins:
+                if "u_e"+str(edges[i].left) + "," + str(edges[i].right)+";"+str(label) in var.name:
+                    bins2.append(var)
+            prob += lpSum(bins2) == u_nodes[edges[i].left][label]
+            bins2 = []
+        for label in range(len(nodes[edges[i].right].costs)):
+            for var in bins:
+                if fnmatch.fnmatch(var.name, "u_e"+str(edges[i].left) + "," + str(edges[i].right)+";"+"*,"+str(label)):
+                    bins2.append(var)
+            prob += lpSum(bins2) == u_nodes[edges[i].right][label]
+            bins2 = []
         u_edges.append(bins)
         bins = []
 
     prob += lpSum(objs)
-    print(prob)
-    print(LpStatus[prob.status])
-    prob.solve()
-    print(LpStatus[prob.status])
-    for v in prob.variables():
-        print(v.name, "=", v.varValue)
     return prob
 
 def ilp_to_labeling(nodes, edges, ilp):
-    solution = ilp.solve
-    print(LpStatus[ilp.status])
+    sol = [0] * len(nodes)
+    for v in ilp.variables():
+        if "u_n" in v.name and v.varValue == 1:
+            sol[int(v.name[3])] = int(v.name[5])
+    energy = evaluate_energy(nodes, edges, sol)
+    return sol
+
+def evaluate_energy(nodes, edges, assignment):
+    e=0         # Total energy of graph
+    for node in range(len(nodes)):
+        e += getattr(nodes[node], 'costs')[assignment[node]]
+    for edge in edges:
+        e += getattr(edge, 'costs')[assignment[getattr(edge, 'left')], assignment[getattr(edge, 'right')]]
+    return e
