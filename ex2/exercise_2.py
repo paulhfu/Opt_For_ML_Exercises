cc244b04fb014268f4fa8e62f95bf970fbe7849a
paulhfu <hiltpa@gmail.com>
2018-11-28 14:00:06 +0100

    ex2.2 done

--- ex2/exercise_2.py
+++ ex2/exercise_2.py
@@ -3,6 +3,12 @@
 # Author: Paul Hilt <mk197@uni-heidelberg.de>
 # Author: Jonas Massa <massajonas@googlemail.com>
 
+# Theoretical Questions:
+# Exercise 2.2:
+# The LP solution has no variables that are no integers. This is why the LP solution, the rounded solution and the ILP
+# solution match exactly. However
+
+
 from pulp import *
 from pulp.solvers import *
 import fnmatch
@@ -10,8 +16,6 @@ import numpy as np
 
 def convert_to_ilp(nodes, edges):
     prob = LpProblem("test1", LpMinimize)
-    solver = CPLEX_CMD(path="/home/paul/tools/ibm/ILOG/CPLEX_Studio128/cplex/bin/x86-64_linux/cplex")
-    prob.setSolver(solver)
 
     # Variables
     u_nodes = []
@@ -58,13 +62,57 @@ def ilp_to_labeling(nodes, edges, ilp):
     for v in ilp.variables():
         if "u_n" in v.name and v.varValue == 1:
             sol[int(v.name[3])] = int(v.name[5])
-    energy = evaluate_energy(nodes, edges, sol)
     return sol
 
-def evaluate_energy(nodes, edges, assignment):
-    e=0         # Total energy of graph
-    for node in range(len(nodes)):
-        e += getattr(nodes[node], 'costs')[assignment[node]]
-    for edge in edges:
-        e += getattr(edge, 'costs')[assignment[getattr(edge, 'left')], assignment[getattr(edge, 'right')]]
-    return e
+def convert_to_lp(nodes, edges):
+    prob = LpProblem("test1", LpMinimize)
+
+    # Variables
+    u_nodes = []
+    objs = []
+    bins = []
+    for i in range(len(nodes)):
+        for j in range(len(nodes[i].costs)):
+            bins.append(LpVariable("u_n"+str(i)+","+str(j), 0, 1, cat=LpContinuous))
+            # Objective
+            objs.append(bins[j] * nodes[i].costs[j])
+        prob += lpSum(bins) == 1
+        u_nodes.append(bins)
+        bins = []
+
+    u_edges = []
+    bins = []
+    for i in range(len(edges)):
+        for key in edges[i].costs:
+            bins.append(LpVariable("u_e"+str(edges[i].left) + "," + str(edges[i].right)+";"+str(key[0])+","+str(key[1])
+                                   , 0, 1, cat=LpContinuous))
+            # Objective
+            objs.append(bins[-1] * edges[i].costs[key])
+        bins2 = []
+        for label in range(len(nodes[edges[i].left].costs)):
+            for var in bins:
+                if "u_e"+str(edges[i].left) + "," + str(edges[i].right)+";"+str(label) in var.name:
+                    bins2.append(var)
+            prob += lpSum(bins2) == u_nodes[edges[i].left][label]
+            bins2 = []
+        for label in range(len(nodes[edges[i].right].costs)):
+            for var in bins:
+                if fnmatch.fnmatch(var.name, "u_e"+str(edges[i].left) + "," + str(edges[i].right)+";"+"*,"+str(label)):
+                    bins2.append(var)
+            prob += lpSum(bins2) == u_nodes[edges[i].right][label]
+            bins2 = []
+        u_edges.append(bins)
+        bins = []
+
+    prob += lpSum(objs)
+    return prob
+
+def lp_to_labeling(nodes, edges, lp):
+    sol = [0] * len(nodes)
+    for n in range(len(nodes)):
+        bin = [0] * len(nodes[n].costs)
+        for v in lp.variables():
+            if "u_n" in v.name and int(v.name[3:v.name.find(',')]) == n:
+                bin[int(v.name[v.name.find(',')+1:len(v.name)])] = v.varValue
+        sol[n] = np.argmax(np.asarray(bin))
+    return sol
