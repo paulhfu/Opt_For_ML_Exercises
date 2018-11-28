bbcca5ec849e4aad96ff1abebe13ba684cdb7c22
paulhfu <hiltpa@gmail.com>
2018-11-28 14:50:04 +0100

    cosmetics

--- ex2/exercise_2.py
+++ ex2/exercise_2.py
@@ -6,11 +6,11 @@
 # Theoretical Questions:
 # Exercise 2.2:
 # The LP solution has no variables that are no integers. This is why the LP solution, the rounded solution and the ILP
-# solution match exactly. However
+# solution match exactly.
 # Exercise 2.3:
 # The LP variables for the nodes of the lp solutions have all the value 0.5. The np.argmax function returns the
 # smallest index of the largest value in an array. This is why the labels of the rounded solution all result to be 0.
-# Whereas the ilp solution is [1, 1, 0]
+# Whereas the ilp solution is [1, 1, 0].
 
 from pulp import *
 from pulp.solvers import *
@@ -37,7 +37,9 @@ def convert_to_ilp(nodes, edges):
     bins = []
     for i in range(len(edges)):
         for key in edges[i].costs:
-            bins.append(LpVariable("u_e"+str(edges[i].left) + "," + str(edges[i].right)+";"+str(key[0])+","+str(key[1])
+            bins.append(LpVariable("u_e" + str(edges[i].left)
+                                   + "," + str(edges[i].right)
+                                   + ";" + str(key[0]) + "," + str(key[1])
                                    , 0, 1, cat=LpInteger))
             # Objective
             objs.append(bins[-1] * edges[i].costs[key])
@@ -45,12 +47,17 @@ def convert_to_ilp(nodes, edges):
         for label in range(len(nodes[edges[i].left].costs)):
             for var in bins:
                 if "u_e"+str(edges[i].left) + "," + str(edges[i].right)+";"+str(label) in var.name:
-                    bins2.append(var)
+                    bins2.append(var)  # Collect here all right edges-variables per node and label
+            # Pose constraint, that the sum of right edges per node and label equals to the value of the variable of
+            # corresponding node and label. This together with the same constraint for the left labels guarantees, that
+            # nodes and edges in the solution correspond to the same labels.
             prob += lpSum(bins2) == u_nodes[edges[i].left][label]
             bins2 = []
         for label in range(len(nodes[edges[i].right].costs)):
             for var in bins:
-                if fnmatch.fnmatch(var.name, "u_e"+str(edges[i].left) + "," + str(edges[i].right)+";"+"*,"+str(label)):
+                if fnmatch.fnmatch(var.name, "u_e" + str(edges[i].left)
+                                             + "," + str(edges[i].right)
+                                             + ";" + "*," + str(label)):
                     bins2.append(var)
             prob += lpSum(bins2) == u_nodes[edges[i].right][label]
             bins2 = []
@@ -87,21 +94,31 @@ def convert_to_lp(nodes, edges):
     bins = []
     for i in range(len(edges)):
         for key in edges[i].costs:
-            bins.append(LpVariable("u_e"+str(edges[i].left) + "," + str(edges[i].right)+";"+str(key[0])+","+str(key[1])
+            bins.append(LpVariable("u_e" + str(edges[i].left)
+                                   + "," + str(edges[i].right) + ";"
+                                   + str(key[0]) + "," + str(key[1])
                                    , 0, 1, cat=LpContinuous))
             # Objective
             objs.append(bins[-1] * edges[i].costs[key])
         bins2 = []
         for label in range(len(nodes[edges[i].left].costs)):
             for var in bins:
-                if "u_e"+str(edges[i].left) + "," + str(edges[i].right)+";"+str(label) in var.name:
-                    bins2.append(var)
+                if "u_e"+str(edges[i].left) + "," \
+                        + str(edges[i].right) + ";" \
+                        + str(label) in var.name:
+                    bins2.append(var)  # Collect here all right edges-variables per node and label
+            # Pose constraint, that the sum of right edges per node and label equals to the value of the variable of
+            # corresponding node and label. This together with the same constraint for the left labels guarantees, that
+            # nodes and edges in the solution correspond to the same labels.
             prob += lpSum(bins2) == u_nodes[edges[i].left][label]
             bins2 = []
         for label in range(len(nodes[edges[i].right].costs)):
             for var in bins:
-                if fnmatch.fnmatch(var.name, "u_e"+str(edges[i].left) + "," + str(edges[i].right)+";"+"*,"+str(label)):
+                if fnmatch.fnmatch(var.name, "u_e"+str(edges[i].left)
+                                             + "," + str(edges[i].right)
+                                             + ";" + "*," + str(label)):
                     bins2.append(var)
+
             prob += lpSum(bins2) == u_nodes[edges[i].right][label]
             bins2 = []
         u_edges.append(bins)
@@ -116,6 +133,6 @@ def lp_to_labeling(nodes, edges, lp):
         bin = [0] * len(nodes[n].costs)
         for v in lp.variables():
             if "u_n" in v.name and int(v.name[3:v.name.find(',')]) == n:
-                bin[int(v.name[v.name.find(',')+1:len(v.name)])] = v.varValue
-        sol[n] = np.argmax(np.asarray(bin))
+                bin[int(v.name[v.name.find(',') + 1:len(v.name)])] = v.varValue  # Collect node variables for each node
+        sol[n] = np.argmax(np.asarray(bin))  # the index of the largest variable corresponds to rounded labeling
     return sol
